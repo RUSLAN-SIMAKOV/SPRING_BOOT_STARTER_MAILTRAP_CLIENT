81a36e7039c0c6d9e5c13b3e962bf3dbb39c0208
Ruslan Simakov <UA667766706@GMAIL.COM>
2024-05-07 21:53:44 +0100

    Initial commit

---
+++ src/main/java/ruslan/simakov/springbootstartermailtrapclient/SpringBootStarterMailtrapClientApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.springbootstartermailtrapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterMailtrapClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterMailtrapClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/springbootstartermailtrapclient/SpringBootStarterMailtrapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.springbootstartermailtrapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterMailtrapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
